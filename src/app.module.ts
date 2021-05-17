08882f9bef4126a903230b89c287b9314ec8d710
Lakhdar Ben boudina <lakhdar.boudina@gmail.com>
2021-05-17 23:41:16 +0100

    added readme and author info

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 
 import dbConfig from './config/dbConfig';
